c8def5730f17ba6a0e9c2f53181f1e23c054ccfd
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-06-17 23:11:29 +0300

    17.06

---
+++ JS1/script.js
@@ -0,0 +1,50 @@
+// "use strict"
+
+// a = b * 2;
+// сутність.length;
+
+// console.log('Bohdana'.length);
+// console.log('Bohdana'.toUpperCase());
+// let age = 25;
+
+
+// age = 30;
+// console.log(ages);
+
+
+
+// const a = 5;
+// const b = 25.5;
+
+
+// const c = 'hello';
+
+// const d = true;
+
+// const isModalOpen = false;
+
+// let selectedProduct = null;
+
+// let userName;
+
+// console.log(userName);
+
+// console.log(typeof a);
+// console.log(typeof userName);
+
+
+
+
+// const message = 'Hello GoITeens';
+// console.log(message);
+
+// const userName = 'Nick';
+// console.log("Username is ", userName);
+// alert(message); 
+
+// const message = confirm("Чи згодні з умовами?");
+// console.log(message);
+
+
+const message = prompt("Вкажіть ваш вік");
+console.log(message);
