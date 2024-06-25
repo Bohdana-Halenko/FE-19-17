982afedf1f8dce8040ba671f6d074f81a381f823
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-06-25 17:57:06 +0300

    24.06

---
+++ JS2/script.js
@@ -0,0 +1,74 @@
+// const a = 20;
+// const b = 5;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// // Оператори порівняння
+// console.log(a > b);
+// console.log(a < b);
+// a >= b;
+// a <= b;
+// a == b;
+// a != b;
+// console.log(a === b);
+// console.log(a !== b);
+
+// 5 == '5' //true
+// 5 === '5' //false
+
+// 0 == false;
+// 0 === false;
+
+// const c = '20';
+// console.log(typeof Number(c));
+
+
+// const d = 'qwe';
+// console.log(Number(d));
+// console.log(typeof Number(d));
+
+
+// console.log(Number.parseInt('10px')); //10
+// console.log(Number.parseInt('15qwe24')); //15
+// console.log(Number.parseInt('14.45qwe')); //14
+// console.log(Number.parseInt('qwe')); //NaN
+
+// console.log(Number.parseFloat('10px')); //10
+// console.log(Number.parseFloat('12qwe74')); //12
+// console.log(Number.parseFloat('12.24qwe78')); //12.24
+
+
+
+
+// const x = Number('25');
+// console.log(Number.isNaN(x)); //false
+
+// const y = Number('qweqwe');
+// console.log(Number.isNaN(y)); //true
+
+// 0.3 + 0.5
+// 0.3 //0.30000000000000002
+
+// // console.log(0.1 + 0.3 === 0.4); 0.40000000000001 === 0.4
+// console.log(0.1 + 0.3); //0.40000000000001
+
+// (0.1*10 + 0.3*10) / 10
+
+
+console.log(Math.floor(2.8)); //2
+
+console.log(Math.ceil(3.4)); //4
+
+console.log(Math.round(1.2)); //1
+console.log(Math.round(1.5)); //2
+
+console.log(Math.max(20, 100, 47, 124, 99)); //124
+
+console.log(Math.min(85, 24, 78, 12)); //12
+
+console.log(Math.pow(2, 3)); //
+
+console.log(Math.random() * (10 - 1) + 1);
