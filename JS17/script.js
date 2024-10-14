8907b705a349d4be99221866046335e4aa6b1600
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-10-14 20:03:52 +0300

    14.10

---
+++ JS17/script.js
@@ -0,0 +1,11 @@
+const title = document.querySelector('.title');
+title.style.color = 'red';
+
+const btn = document.querySelectorAll
+btn.style.backgroundColor = 'skyblue';
+btn.style.color = 'white';
+btn.style.padding = '10px 20px';
+btn.style.borderRadius = '20px';
+btn.style.border = 'none';
+
+
