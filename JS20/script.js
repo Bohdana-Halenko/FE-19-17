dfb1521c5b41c346c3980b09de894bcee5d2f10c
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-11-04 21:00:21 +0200

    04.11

---
+++ JS20/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// document.addEventListener("keydown", event => {
+//     console.log("Keydown:", event);
+// });
+
+// document.addEventListener("keyup", event => {
+//     console.log("keyUp:", event)
+// })
+
+// document.addEventListener("keydown", event => {
+//     console.log('key:', event.key);
+//     console.log('code:', event.code);
+// })
+
+// document.addEventListener("keydown", event => {
+//     event.preventDefault();
+//     if ((event.ctrlKey || event.metaKey || event.shiftKey) && event.code === "KeyS") {
+//         console.log("Ctrl+s or Command+s combo")
+// }
+// })
