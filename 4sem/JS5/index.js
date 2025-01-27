2ca36b34863b1e8b539e5d4764a255018861b2aa
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-01-27 20:59:01 +0200

    27/01

---
+++ 4sem/JS5/index.js
@@ -0,0 +1,15 @@
+// const newM = require('./test');
+
+// console.log(newM.hello());
+// console.log(newM.bye())
+
+
+
+// import { square, diag } from "./test";
+// import * as myModule from '.test';
+
+// console.log(myModule.square(11)); //121
+// console.log(myModule.diag(4, 3));
+
+import one from './test';
+one();
---
+++ 4sem/JS5/test.js
@@ -0,0 +1,22 @@
+// const helloM = 'hello!';
+// const byeM = 'Goodbye';
+
+// const hello = () => helloM;
+// const bye = () => byeM;
+
+// module.exports = {
+//     hello,
+//     bye,
+// }
+
+
+
+
+// const sqrt = Math.sqrt;
+// const square = x => x * x;
+// const diag = (x, y) => sqrt(square(x) + square(y));
+
+// export { square, diag };
+
+
+export default function one(){...}
